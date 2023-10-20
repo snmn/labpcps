720be58bed758e5c409317e7861988f47d0e6b75
seaso <seasonmaharjan@kathmandu.codes>
2023-10-20 08:40:34 +0545

    test

--- lib/static.dart
+++ lib/static.dart
@@ -2,8 +2,6 @@
 class StaticValue{
   //static
   static String baseurl = "newsapi.org";
-  //https://newsapi.org/v2/everything?q=apple&from=2023-10-16&to=2023-10-16
-  // &sortBy=popularity&apiKey=a25a0c0b883c4f389ff5c23da0f9dfea
+  //https://newsapi.org/v2/everything&apiKey=a25a0c0b883c4f389ff5c23da0f9dfea
   static String apikey = "a25a0c0b883c4f389ff5c23da0f9dfea";
-
 }
